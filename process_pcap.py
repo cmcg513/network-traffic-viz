314289b3d15c0f651c7465cf3a9513d01af09b48
Casey McGinley <cmcg513@gmail.com>
2016-04-30 21:41:15 -0400

    added sent received stats

--- process_pcap.py
+++ process_pcap.py
@@ -46,41 +46,68 @@ def update_agg_stats(stats,ip,protoc):
 		stats['agg']['per_protoc'][protoc] = 1
 
 def update_per_ip_stats(stats,ip,protoc):
-	# update for ip.src
-	update_per_ip_stats_sub(stats,ip.src,ip.dst,protoc)
-	# update for ip.dst
-	update_per_ip_stats_sub(stats,ip.dst,ip.src,protoc)
+	if not(ip.src in stats['per_ip']):
+		init_per_ip(stats,ip.src)
+	if not(ip.dst in stats['per_ip']):
+		init_per_ip(stats,ip.dst)
 
-def update_per_ip_stats_sub(stats,ip_addr1,ip_addr2,protoc):
-	if not(ip_addr1 in stats['per_ip']):
-		init_per_ip(stats,ip_addr1)
-	ip_stats = stats['per_ip'][ip_addr1]
-	ip_stats['agg']['total'] += 1
-	if protoc in ip_stats['agg']['per_protoc']:
-		ip_stats['agg']['per_protoc'][protoc] += 1
+	src_stats = stats['per_ip'][ip.src]
+	dst_stats = stats['per_ip'][ip.dst]
+	src_stats['agg']['total']['sent'] += 1
+	dst_stats['agg']['total']['sent'] += 1
+	# src_stats['agg']['total']['rcvd'] += 1
+	if protoc in src_stats['agg']['per_protoc']:
+		src_stats['agg']['per_protoc'][protoc]['sent'] += 1
+		# src_stats['agg']['per_protoc'][protoc]['rcvd'] += 1
 	else:
-		ip_stats['agg']['per_protoc'][protoc] = 1
-
-	if ip_addr2 not in ip_stats['per_other_ip']:
-		init_per_other_ip(stats,ip_addr1,ip_addr2)
-
-	other_ip_stats = ip_stats['per_other_ip'][ip_addr2]
-	other_ip_stats['total'] += 1
-	if protoc in other_ip_stats['per_protoc']:
-		other_ip_stats['per_protoc'][protoc] += 1
+		src_stats['agg']['per_protoc'][protoc] = dict()
+		src_stats['agg']['per_protoc'][protoc]['sent'] = 1
+		src_stats['agg']['per_protoc'][protoc]['rcvd'] = 0
+		# src_stats['agg']['per_protoc'][protoc]['rcvd'] = 1
+	if protoc in dst_stats['agg']['per_protoc']:
+		# src_stats['agg']['per_protoc'][protoc]['sent'] += 1
+		dst_stats['agg']['per_protoc'][protoc]['rcvd'] += 1
 	else:
-		other_ip_stats['per_protoc'][protoc] = 1
+		# src_stats['agg']['per_protoc'][protoc]['sent'] = 1
+		dst_stats['agg']['per_protoc'][protoc] = dict()
+		dst_stats['agg']['per_protoc'][protoc]['sent'] = 0
+		dst_stats['agg']['per_protoc'][protoc]['rcvd'] = 1
+
+	if ip.dst not in src_stats['per_other_ip']:
+		init_per_other_ip(stats,ip.src,ip.dst)
+	if ip.src not in dst_stats['per_other_ip']:
+		init_per_other_ip(stats,ip.dst,ip.src)
+
+	# other = src_stats['per_other_ip'][ip.dst]
+	src_stats['per_other_ip'][ip.dst]['total']['sent'] += 1
+	dst_stats['per_other_ip'][ip.src]['total']['rcvd'] += 1
+	if protoc in src_stats['per_other_ip'][ip.dst]['per_protoc']:
+		src_stats['per_other_ip'][ip.dst]['per_protoc'][protoc]['sent'] += 1
+	else:
+		src_stats['per_other_ip'][ip.dst]['per_protoc'][protoc] = dict()
+		src_stats['per_other_ip'][ip.dst]['per_protoc'][protoc]['sent'] = 1
+		src_stats['per_other_ip'][ip.dst]['per_protoc'][protoc]['rcvd'] = 0
+	if protoc in dst_stats['per_other_ip'][ip.src]['per_protoc']:
+		dst_stats['per_other_ip'][ip.src]['per_protoc'][protoc]['rcvd'] += 1
+	else:
+		dst_stats['per_other_ip'][ip.src]['per_protoc'][protoc] = dict()
+		dst_stats['per_other_ip'][ip.src]['per_protoc'][protoc]['sent'] = 0
+		dst_stats['per_other_ip'][ip.src]['per_protoc'][protoc]['rcvd'] = 1
 
 def init_per_ip(stats,ip_addr):
 	stats['per_ip'][ip_addr] = dict()
 	stats['per_ip'][ip_addr]['agg'] = dict()
-	stats['per_ip'][ip_addr]['agg']['total'] = 0
+	stats['per_ip'][ip_addr]['agg']['total'] = dict()
+	stats['per_ip'][ip_addr]['agg']['total']['sent'] = 0
+	stats['per_ip'][ip_addr]['agg']['total']['rcvd'] = 0
 	stats['per_ip'][ip_addr]['agg']['per_protoc'] = dict()
 	stats['per_ip'][ip_addr]['per_other_ip'] = dict()
 
 def init_per_other_ip(stats,ip_addr1,ip_addr2):
 	stats['per_ip'][ip_addr1]['per_other_ip'][ip_addr2] = dict()
-	stats['per_ip'][ip_addr1]['per_other_ip'][ip_addr2]['total'] = 0
+	stats['per_ip'][ip_addr1]['per_other_ip'][ip_addr2]['total'] = dict()
+	stats['per_ip'][ip_addr1]['per_other_ip'][ip_addr2]['total']['sent'] = 0
+	stats['per_ip'][ip_addr1]['per_other_ip'][ip_addr2]['total']['rcvd'] = 0
 	stats['per_ip'][ip_addr1]['per_other_ip'][ip_addr2]['per_protoc'] = dict()
 
 if __name__ == "__main__":
